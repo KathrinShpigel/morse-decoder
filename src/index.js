92d5472203d16862181b292d0ee6b3b44583da91
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2020-10-08 00:14:59 +0300

    feat: add morse-decoder solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const letters = [];
+    let sign = '';
+
+    for (i = 0; (i + 10) <= expr.length; i += 10) {
+        sign = expr.substring(i, i + 10);
+        
+        sign === "**********" ? letters.push(" ") : letters.push(Number(sign).toString());
+    };
+
+
+    for (i = 0; i < letters.length; i++) {
+        letters[i] = letters[i]
+            .replace(/10/g, '.')
+            .replace(/11/g, '-')
+            .replace(/([.-]+[-.]*)/g, (_, x) => MORSE_TABLE[x]);
+    };
+
+    return letters.join('');
 }
 
 module.exports = {
